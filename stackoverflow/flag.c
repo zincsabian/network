b1ec02c2d597b18e9e442f08331de87f77e40d0d
zincsabian <40512232+zincsabian@users.noreply.github.com>
2023-12-18 18:59:55 +0800

    Update flag.c
    update && try to catch the different mem

--- stackoverflow/flag.c
+++ stackoverflow/flag.c
@@ -3,16 +3,16 @@
 
 int main(int argc, char const *argv[])
 {
-    char flag='0'; 
-    char arr[16]; // buffer size is too small
+    char flag=0; 
+    char arr[64]; // buffer size is too small
     char *ptr;
     ptr = getenv("PWD"); // an unset environment variable
-    printf("%zu %zu\n", (size_t)(&flag), (size_t)(&(arr[0])));
     if(ptr != NULL)
     {
         sprintf(arr, "Present working directroy is: %s\n", ptr);
         printf("%s", arr);
     }
+    printf("%zu %zu\n", (size_t)(&flag), (size_t)(&(arr[0])));
     if(flag == 0) 
     {
         printf("Flag is not here\n");
